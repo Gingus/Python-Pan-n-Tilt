62b2ff7791caa1d828c16f2e71ca1f5074c80555
Gingus <pminard1@outlook.com>
2019-11-18 20:25:28 +0000

    Version 2 working version from pi zero

--- test3.py
+++ test3.py
@@ -14,7 +14,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '../'))
 
 import pygubu
 #Ucomment on pi
-#from board import SCL, SDA
+from board import SCL, SDA
 import busio
 
 # Import the PCA9685 module.
@@ -22,17 +22,17 @@ from adafruit_pca9685 import PCA9685
 #This uses Adafruit motor library, note: good for continuous rotation servos
 from adafruit_motor import servo
 
-#i2c = busio.I2C(SCL, SDA)
+i2c = busio.I2C(SCL, SDA)
 
 # Create a simple PCA9685 class instance.
-#pca = PCA9685(i2c)
-#pca.frequency = 50
+pca = PCA9685(i2c)
+pca.frequency = 50
 
 # To get the full range of the servo you will likely need to adjust the min_pulse and max_pulse to
 # match the stall points of the servo.
 # This is an example for the Sub-micro servo: https://www.adafruit.com/product/2201
-#####servo1 = servo.Servo(pca.channels[1], min_pulse=580, max_pulse=2480)
-
+servo1 = servo.Servo(pca.channels[0], min_pulse=580, max_pulse=2480)
+servo2 = servo.Servo(pca.channels[1], min_pulse=580, max_pulse=2480)
 # This is an example for the Micro Servo - High Powered, High Torque Metal Gear:
 #   https://www.adafruit.com/product/2307
 #servo1 = servo.Servo(pca.channels[1], min_pulse=600, max_pulse=2400)
@@ -44,8 +44,10 @@ from adafruit_motor import servo
 # servo7 = servo.Servo(pca.channels[7], min_pulse=600, max_pulse=2600)
 
 # The pulse range is 1000 - 2000 by default.
-#####servo1 = servo.Servo(pca.channels[1])
-
+servo1 = servo.Servo(pca.channels[0])
+servo2 = servo.Servo(pca.channels[1])
+servo1.angle = 50
+servo2.angle = 50
 ##for i in range(180):
 ##    servo1.angle = i
 ##for i in range(180):
@@ -104,30 +106,30 @@ class Myapp:
 ##    def checkbutton_command(self):
 ##        messagebox.showinfo('Title', 'Checkbutton command')
 
-    def on_scale1_changed(self, event):#This matches the command for 		scale1
+    def on_scale1_changed(self, event):#This matches the command for        scale1
         #in gubu. Gets the scale's command
         label = self.builder.get_object('Label_Tilt_Scale')
         #Gets and builds the label by name
-        scale = self.builder.get_object('Scale_Tilt')#'scale1' = the ID in 		gubu
+        scale1 = self.builder.get_object('Scale_Tilt')#'scale1' = the ID in      gubu
         #of the scale in the UI file. Gets and builds the scale
-        label.configure(text=scale.get())
+        label.configure(text=scale1.get())
         #Uses the variable from the scale  and outputs it to the label
-        print(scale.get()) #New Test Worked
+        print(scale1.get()) #New Test Worked
         #uses the variable from scale to update the servo position
-        #servo1.angle = scale.get()
+        servo1.angle = scale1.get()
         #pca.deinit()#New Bit! Removed this as it stops the servo updating/moving
     
-    def on_scale2_changed(self, event):#This matches the command for 		scale1
+    def on_scale2_changed(self, event):#This matches the command for        scale1
         #in gubu. Gets the scale's command
         label = self.builder.get_object('Label_Pan_Scale')
         #Gets and builds the label by name
-        scale = self.builder.get_object('Scale_Pan')#'scale1' = the ID in 		gubu
+        scale2 = self.builder.get_object('Scale_Pan')#'scale1' = the ID in       gubu
         #of the scale in the UI file. Gets and builds the scale
-        label.configure(text=scale.get())
+        label.configure(text=scale2.get())
         #Uses the variable from the scale  and outputs it to the label
-        print(scale.get()) #New Test Worked
+        print(scale2.get()) #New Test Worked
         #uses the variable from scale to update the servo position
-        #servo2.angle = scale.get()
+        servo2.angle = scale2.get()
         #pca.deinit()#New Bit! Removed this as it stops the servo updating/moving
     
     
